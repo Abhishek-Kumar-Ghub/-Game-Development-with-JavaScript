b211fa5a2a95951169dd30b2cdda99840db14fb2
Abhishek Kumar <153679537+Abhishek-Kumar-Ghub@users.noreply.github.com>
2024-07-30 12:59:37 +0530

    Add files via upload

---
+++ Password Generator/script.js
@@ -0,0 +1,36 @@
+var btn = document.querySelector("button");
+var h3 = document.querySelector("h3");
+
+btn.addEventListener("click", function(){
+ const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//  Defines a string containing all uppercase letters.
+ const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+ const numbers = "0123456789";
+ const specialCharacters = "!@#$%^&*()-_+=<>?";
+
+ let uppercase =
+   uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+  //  Selects a random uppercase letter from the uppercaseLetters string.
+
+ let numeric = "";
+ for (let i = 0; i < 2; i++) {
+   numeric += numbers[Math.floor(Math.random() * numbers.length)];
+ }
+
+ let lower =
+   lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
+
+ let special =
+   specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+
+ let remaining = "";
+ for (let i = 0; i < 3; i++) {
+   const charset =
+     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}[]<>?/|";
+   remaining += charset.charAt(Math.floor(Math.random() * charset.length));
+ }
+
+ let password = uppercase + numeric + lower + special + remaining;
+ h3.innerHTML=password;
+ h3.style.opacity=1;
+});
